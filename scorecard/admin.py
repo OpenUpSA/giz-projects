f4d5cdb64442e1070df7e5191db3cf93c75e1061
Adi Eyal <adi@openup.org.za>
2018-12-04 21:28:45 +0200

    Import/Export now uses natural keys for foreign key fields

--- scorecard/admin.py
+++ scorecard/admin.py
@@ -2,18 +2,33 @@ import tablib
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
+from import_export.fields import Field
 
-from .models import Project, Programme
+from .models import Project, Programme, Geography
+
+
+class ProgrammeForeignKeyWidget(ForeignKeyWidget):
+    def render(self, value, obj=None):
+        import pdb; pdb.set_trace()
 
 class ProjectResource(resources.ModelResource):
 
+    programme = Field(
+        column_name='programme',
+        attribute='programme',
+        widget=ForeignKeyWidget(Programme, 'name')
+    )
+
+    geo = Field(
+        column_name='municipality',
+        attribute='geo',
+        widget=ForeignKeyWidget(Geography, 'geo_code')
+    )
+
     class Meta:
         model = Project
-
-ProjectResource = resources.modelresource_factory(model=Project)
-project_resource = ProjectResource()
-dataset = tablib.Dataset(['1', '', 'New book'], headers=['id', 'title', 'programme'])
-result = project_resource.import_data(dataset, dry_run=True)
+        export_order = ('id', 'programme', 'geo', 'title', 'status', 'area_of_work', 'mode_of_delivery', 'partner', 'agenda', 'm_and_e', 'contact', 'email')
 
 class ProjectAdmin(ImportExportModelAdmin):
     resource_class = ProjectResource
