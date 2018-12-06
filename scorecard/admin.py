95e1357f90e35e38e341d7ff840439b990dc718a
Adi Eyal <adi@openup.org.za>
2018-12-06 09:28:44 +0200

    Tweaks to admin interface

--- scorecard/admin.py
+++ scorecard/admin.py
@@ -1,4 +1,3 @@
-import tablib
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
@@ -7,7 +6,6 @@ from import_export.fields import Field
 
 from .models import Project, Programme, Geography
 
-
 class CleanForeignKeyWidget(ForeignKeyWidget):
     def clean(self, value, row, *args, **kwargs):
         if hasattr(value, "strip"):
@@ -15,7 +13,6 @@ class CleanForeignKeyWidget(ForeignKeyWidget):
         return super(CleanForeignKeyWidget, self).clean(value, row, *args, **kwargs)
 
 class ProjectResource(resources.ModelResource):
-
     programme = Field(
         column_name='programme',
         attribute='programme',
@@ -30,13 +27,17 @@ class ProjectResource(resources.ModelResource):
 
     class Meta:
         model = Project
-        export_order = ('id', 'programme', 'geo', 'title', 'status', 'area_of_work', 'mode_of_delivery', 'partner', 'agenda', 'm_and_e', 'contact', 'email')
+        export_order = (
+            'id', 'programme', 'geo', 'title', 'status', 
+            'area_of_work', 'mode_of_delivery', 'partner',
+            'agenda', 'm_and_e', 'contact', 'email'
+        )
 
 class ProjectAdmin(ImportExportModelAdmin):
     resource_class = ProjectResource
     list_display = ['geo', 'programme', 'title', 'status']
     search_fields = ['title']
-    list_filter = ['status', 'geo__name']
+    list_filter = ['status', 'programme', 'geo__name']
 
 class ProgrammeAdmin(admin.ModelAdmin):
     pass
