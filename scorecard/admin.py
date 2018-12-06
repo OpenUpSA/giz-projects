21dcf99d62ded6c18198447e77d0f132177e6a5a
Adi Eyal <adi@openup.org.za>
2018-12-06 12:00:44 +0200

    Using select2 for municipality search

--- scorecard/admin.py
+++ scorecard/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django import forms
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import ForeignKeyWidget
 from import_export.fields import Field
+#from django_extensions.admin import ForeignKeyAutocompleteAdmin
 
 from .models import Project, Programme, Geography
 
@@ -33,11 +35,23 @@ class ProjectResource(resources.ModelResource):
             'agenda', 'm_and_e', 'contact', 'email'
         )
 
+
+class GeoForm(forms.ModelForm):
+    geo = forms.ModelChoiceField(queryset=Geography.objects.order_by('name'))
+
+    class Meta:
+        model = Geography
+        fields = '__all__'
+
 class ProjectAdmin(ImportExportModelAdmin):
     resource_class = ProjectResource
     list_display = ['geo', 'programme', 'title', 'status']
     search_fields = ['title']
     list_filter = ['status', 'programme', 'geo__name']
+    autocomplete_fields = ['geo']
+    form = GeoForm
+
+#ForeignKeyAutocompleteAdmin
 
 class ProgrammeAdmin(admin.ModelAdmin):
     pass
