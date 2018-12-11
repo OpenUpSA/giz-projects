6c6cf7275dac531c9b0abf30840da76a3603f0e3
Adi Eyal <adi@openup.org.za>
2018-12-11 14:57:57 +0200

    Added test for municipalities api

--- scorecard/test_views.py
+++ scorecard/test_views.py
@@ -2,8 +2,10 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
         
-class TestProjectsApi(TestCase):
+class TestApi(TestCase):
     fixtures = ['initial_data.json']
+
+class TestProjectsApi(TestApi):
 
     def testReverseUrl(self):
         url = reverse("projects_api")
@@ -19,3 +21,17 @@ class TestProjectsApi(TestCase):
         self.assertEquals(type(js), list)
         self.assertGreater(len(js), 0)
 
+class TestMunicipalitiesApi(TestApi):
+    def testReverseUrl(self):
+        url = reverse("municipalities_api")
+        self.assertEquals(url, "/api/municipalities")
+
+    def testProjectsApi(self):
+        url = reverse("municipalities_api")
+        c = Client()
+        response = c.get(url)
+        self.assertEquals(response.status_code, 200)
+
+        js = response.json()
+        self.assertEquals(type(js), list)
+        self.assertGreater(len(js), 0)
