0a11a1bb15c4bce1e229b3fb19c2648e771115f2
Adi Eyal <adi@openup.org.za>
2018-12-14 22:19:52 +0200

    Removed google analytics context processor

--- scorecard/settings.py
+++ scorecard/settings.py
@@ -135,7 +135,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 "django.contrib.messages.context_processors.messages",
-                "scorecard.context_processors.google_analytics",
             ]
         }
     },
